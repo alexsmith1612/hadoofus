298ff26eaf4ac5927f93ed532c9433eddc1484be
Paul Scott <paul.scotto@gmail.com>
2014-05-07 17:59:24 +0100

    Custom setup.py libhadoofus clib building
    Integrates a new build_clib command which supports additional arguments
    in the setup(libraries=[...]) in order to compile on gcc. The Makefile
    is used to pull in some of the compiler arguments using
    distutils.sysconfig.parse_makefile which *cannot* read CFLAGS, LDFLAGS,
    or CPPFLAGS by design, thus we declare PY_{C,LD,CPP}FLAGS.

--- wrappers/py/setup.py
+++ wrappers/py/setup.py
@@ -1,8 +1,48 @@
+from distutils import log
 from distutils.core import setup
+from distutils.command.build_clib import build_clib
 from distutils.extension import Extension
 from distutils.sysconfig import parse_makefile
 from os.path import abspath, dirname, realpath, splitext
 from glob import glob
+
+
+class build_libhadoofus_clib(build_clib):
+    def build_library(self, lib_name, build_info):
+        sources = build_info.get('sources')
+        if sources is None or not isinstance(sources, (list, tuple)):
+            raise DistutilsSetupError(
+                   "in 'libraries' option (library '%s'), "
+                   "'sources' must be present and must be "
+                   "a list of source filenames" % lib_name)
+        sources = list(sources)
+
+        log.info("building '%s' library", lib_name)
+
+        # First, compile the source code to object files in the library
+        # directory.  (This should probably change to putting object
+        # files in a temporary build directory.)
+        macros = build_info.get('macros')
+        include_dirs = build_info.get('include_dirs')
+        extra_args = build_info.get('extra_compile_preargs')
+        objects = self.compiler.compile(sources,
+                                        output_dir=self.build_temp,
+                                        macros=macros,
+                                        include_dirs=include_dirs,
+                                        extra_preargs=extra_args,
+                                        debug=self.debug)
+
+        # Now "link" the object files together into a static library.
+        # (On Unix at least, this isn't really linking -- it just
+        # builds an archive.  Whatever.)
+        self.compiler.create_static_lib(objects, lib_name,
+                                        output_dir=self.build_clib,
+                                        debug=self.debug)
+
+    def build_libraries(self, libraries):
+        for (lib_name, build_info) in libraries:
+            self.build_library(lib_name, build_info)
+
 
 try:
     from Cython.Build import cythonize
@@ -28,7 +68,8 @@ include_dirs = [realpath(dirname(abspath(__file__)) + "/../../include")]
 make_vars = parse_makefile(realpath(dirname(abspath(__file__)) + "/../../src/Makefile"))
 libhadoofus = ("hadoofus", {
     "sources": [realpath(p) for p in glob("%s/../../src/*.c" % dirname(abspath(__file__)))],
-    "extra_compile_args": make_vars["FLAGS"].split()
+    "include_dirs": include_dirs,
+    "extra_compile_preargs": make_vars["FLAGS"].split()
 })
 
 
@@ -64,5 +105,8 @@ implementations.
             include_dirs=include_dirs,
         )
     ]),
-    libraries=[libhadoofus]
+    libraries=[libhadoofus],
+    cmdclass={
+        'build_clib': build_libhadoofus_clib,
+    }
 )
