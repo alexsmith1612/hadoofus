75bec4667162dd90f3c7d92adf8d7d2c72f7c1ac
Tom Arnfeld <tarnfeld@me.com>
2014-05-07 16:01:38 +0100

    PEP8 and other bits

--- wrappers/py/setup.py
+++ wrappers/py/setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from os import getcwd
-from os.path import abspath, dirname, realpath
+from distutils.sysconfig import parse_makefile
+from os.path import abspath, dirname, realpath, splitext
+from glob import glob
 
 try:
     from Cython.Build import cythonize
@@ -10,7 +11,7 @@ except ImportError:
         for extension in extensions:
             sources = []
             for sfile in extension.sources:
-                path, ext = os.path.splitext(sfile)
+                path, ext = splitext(sfile)
                 if ext in ('.pyx', '.py'):
                     if extension.language == 'c++':
                         ext = '.cpp'
@@ -22,47 +23,47 @@ except ImportError:
         return extensions
 
 include_dirs = [realpath(dirname(abspath(__file__)) + "/../../include")]
+
+make_vars = parse_makefile(realpath(dirname(abspath(__file__)) + "/../../src/Makefile"))
 ext_modules = cythonize([
     Extension(
-        "hadoofus",
-        ["hadoofus.pyx",
-         realpath(dirname(abspath(__file__)) + "/../../src/datanode.c"),
-         realpath(dirname(abspath(__file__)) + "/../../src/heapbuf.c"),
-         realpath(dirname(abspath(__file__)) + "/../../src/heapbufobjs.c"),
-         realpath(dirname(abspath(__file__)) + "/../../src/highlevel.c"),
-         realpath(dirname(abspath(__file__)) + "/../../src/namenode.c"),
-         realpath(dirname(abspath(__file__)) + "/../../src/net.c"),
-         realpath(dirname(abspath(__file__)) + "/../../src/objects.c"),
-         realpath(dirname(abspath(__file__)) + "/../../src/pthread_wrappers.c"),
-         realpath(dirname(abspath(__file__)) + "/../../src/util.c")],
+        name="libhadoofus",
+        sources=[realpath(p) for p in glob("%s/../../src/*.c" % dirname(abspath(__file__)))],
+        include_dirs=include_dirs,
+        extra_compile_args=make_vars["FLAGS"].split()
+    ),
+    Extension(
+        name="hadoofus",
+        sources=["hadoofus.pyx"],
         libraries=["z", "sasl2"],
         include_dirs=include_dirs
     )
 ])
 
+
 setup(
-        name = "hadoofus",
-        version = "0",
-        description = "Python client API for HDFS",
-        author = "Conrad Meyer",
-        author_email = "conrad.meyer@isilon.com",
-        long_description = """
+    name="hadoofus",
+    version="0",
+    description="Python client API for HDFS",
+    author="Conrad Meyer",
+    author_email="conrad.meyer@isilon.com",
+    long_description="""
 This 'hadoofus' python module provides a client API for accessing instances
 of the Apache Hadoop Distributed File System, as well as compatible
 implementations.
 """,
-        url = "<none yet>",
-        classifiers = [
-            'Development Status :: 2 - Pre-Alpha',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: POSIX',
-            'Programming Language :: Cython',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: System :: Filesystems',
-            'Topic :: System :: Networking',
-            ],
-        platforms = ['Posix'],
-        license = 'MIT',
-        ext_modules = ext_modules
+    url="https://github.com/cemeyer/hadoofus",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Cython',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Filesystems',
+        'Topic :: System :: Networking',
+    ],
+    platforms=['Posix'],
+    license='MIT',
+    ext_modules=ext_modules
 )
