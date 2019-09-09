b4b8358c35284d556c86bb2cd3e1ea58c6ce99ee
Alex Smith <aes7mv@virginia.edu>
2019-09-09 11:13:17 -0400

    Change _be32dec() and _be32enc() to static inline functions

--- src/util.c
+++ src/util.c
@@ -15,32 +15,6 @@
 
 #include "util.h"
 
-uint32_t
-_be32dec(void *void_p)
-{
-	uint8_t *p = void_p;
-	uint32_t res;
-
-	res =
-	    ((uint32_t)p[0] << 24) |
-	    ((uint32_t)p[1] << 16) |
-	    ((uint32_t)p[2] << 8) |
-	    (uint32_t)p[3];
-
-	return res;
-}
-
-void
-_be32enc(void *void_p, uint32_t v)
-{
-	uint8_t *p = void_p;
-
-	p[0] = (uint8_t)(v >> 24);
-	p[1] = (uint8_t)((v >> 16) & 0xff);
-	p[2] = (uint8_t)((v >> 8) & 0xff);
-	p[3] = (uint8_t)(v & 0xff);
-}
-
 uint64_t
 _now_ms(void)
 {
--- src/util.h
+++ src/util.h
@@ -59,8 +59,31 @@ _min(off_t a, off_t b)
 	return b;
 }
 
-uint32_t	_be32dec(void *);
-void		_be32enc(void *, uint32_t);
+static inline uint32_t
+_be32dec(void *void_p)
+{
+	uint8_t *p = void_p;
+	uint32_t res;
+
+	res =
+	    ((uint32_t)p[0] << 24) |
+	    ((uint32_t)p[1] << 16) |
+	    ((uint32_t)p[2] << 8) |
+	    (uint32_t)p[3];
+
+	return res;
+}
+
+static inline void
+_be32enc(void *void_p, uint32_t v)
+{
+	uint8_t *p = void_p;
+
+	p[0] = (uint8_t)(v >> 24);
+	p[1] = (uint8_t)((v >> 16) & 0xff);
+	p[2] = (uint8_t)((v >> 8) & 0xff);
+	p[3] = (uint8_t)(v & 0xff);
+}
 
 uint64_t	_now_ms(void);
 
