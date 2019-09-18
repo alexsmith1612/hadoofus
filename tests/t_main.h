d1b278ddc4e0a28f807af51577aecb3634ceb55a
Alex Smith <aes7mv@virginia.edu>
2019-09-18 17:08:12 -0400

    Update tests: current API, sane version handling and test server config
    - Only execute tests for one HDFS version during a given
      invocation (it doesn't make sense to use multiple versions when
      given a single namenode address)
    
    - Add environment variables to configure the namenode version, port,
      and kerberos settings
    
    - Allow libcheck's fork mode to be configured by it's environment
      variable CK_FORK (helpful for use with valgrind)
    
    - Print the type string in addition to the message on exceptions
    
    - Fix memory leaks in tests
        - hdfs_create() used to be void return but now returns an
          allocated file status object
        - Allocated buffers now freed (to clean up valgrind messages)
    
    - Conform tests to the highlevel functions returning NULL instead of
      H_NULL objects now
    
    - Update datanode tests to new datanode API
    
    - Update datanode tests to be agnostic to the TOWRITE size and
      blocksize (i.e. write in a loop until all data is written rather
      than two hardcoded writes)
    
    - Change datanode test fixtures to be checked instead of
      unchecked (i.e. run within the forked address spaces rather than in
      the test program's address space) in order to prevent the tests from
      using the same namenode connection but without maintaining msgno
      state between tests (i.e. to prevent exception-causing msgno
      resuse).
    
    - Add a datanode append test

--- tests/t_main.h
+++ tests/t_main.h
@@ -5,15 +5,18 @@
 # define nelem(ARR) (sizeof(ARR) / sizeof(ARR[0]))
 #endif
 
-#define HDFS_T_ENV "HDFS_TEST_NODE_ADDRESS"
+#define HDFS_T_ADDR "HDFS_TEST_NODE_ADDRESS"
+#define HDFS_T_PORT "HDFS_TEST_NODE_PORT"
 #define HDFS_T_USER "HDFS_TEST_USER"
+#define HDFS_T_VER "HDFS_TEST_VER"
+#define HDFS_T_KERB "HDFS_TEST_KERB"
 
 Suite *		t_hl_rpc_basics_suite(void);
-Suite *		t_hl_rpc2_basics_suite(void);
 Suite *		t_datanode_basics_suite(void);
-Suite *		t_datanode2_basics_suite(void);
 Suite *		t_unit(void);
 
-extern const char *H_ADDR, *H_USER;
+extern const char *H_ADDR, *H_USER, *H_PORT;
+extern enum hdfs_namenode_proto H_VER;
+extern enum hdfs_kerb H_KERB;
 
 #endif
